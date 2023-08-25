8c9944043089925ab6ef038a9e80bc0f80c88ef8
Roman MURZAC <34145412+romanmurzac@users.noreply.github.com>
2023-08-25 14:16:35 +0300

    Delete sharepoint-migration-tool/sharepoint-cloud-tool directory

--- sharepoint-migration-tool/sharepoint-cloud-tool/credentials.py
+++
@@ -1,8 +0,0 @@
-# Provide active user name that has access permisions
-username = "user.name@example.com"
-
-# Provide valid password of the account
-password = "Password"
-
-# Provide company / personal SharePoint site URL
-site_url = "https://<enterprise>.sharepoint.com/sites/<enterprise site>"
